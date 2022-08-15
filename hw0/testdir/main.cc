ce37c3b00c41dbaeff5c42a2d47a90b7ec41bca2
jstrande99 <93735453+jstrande99@users.noreply.github.com>
2022-08-15 00:09:34 -0600

    hw0

---
+++ hw0/testdir/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+        using namespace std;
+
+        int main() {
+            // How many CU students does it take to change a light bulb?
+            cout << "Zero because they have call CSU students to do it for them!\n";
+            return 0;
+        }
