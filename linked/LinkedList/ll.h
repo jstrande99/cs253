7bfdc4427ff0d01b3f418e1724dae2f638234f57
jstrande99 <93735453+jstrande99@users.noreply.github.com>
2022-08-15 00:40:07 -0600

    labs added

---
+++ linked/LinkedList/ll.h
@@ -0,0 +1,19 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+#include <iostream> 
+
+class LinkedList {
+  public:
+    void initialize();
+    bool insert(unsigned uiData);
+    bool remove(unsigned *pData);
+
+  private:
+    struct Link {
+	unsigned m_uiData;
+	Link *m_pNext;
+	void initialize(unsigned uiData, Link *pNext);
+    } *m_pHead;
+};
+
+#endif /* LINKED_LIST_H */ 
